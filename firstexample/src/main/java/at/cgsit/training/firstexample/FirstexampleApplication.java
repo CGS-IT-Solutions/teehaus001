109583f9b029b94c04bc86e5a7f12aafb213505a
ext_christian.schaef <christian.schaefer@cgs.at>
2020-10-12 23:11:36 +0200

    #1 initial

--- firstexample/src/main/java/at/cgsit/training/firstexample/FirstexampleApplication.java
+++ firstexample/src/main/java/at/cgsit/training/firstexample/FirstexampleApplication.java
@@ -1,12 +1,21 @@
 package at.cgsit.training.firstexample;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication public class FirstexampleApplication {
+@SpringBootApplication
+public class FirstexampleApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(FirstexampleApplication.class, args);
+
+    //simple startup version
+    //SpringApplication.run(FirstexampleApplication.class, args);
+
+    SpringApplication app = new SpringApplication(FirstexampleApplication.class);
+    app.setBannerMode(Banner.Mode.OFF);
+    app.run(args);
+
   }
 
 }
---
+++ firstexample/src/main/java/at/cgsit/training/firstexample/rest/SimpleRestController.java
@@ -0,0 +1,31 @@
+package at.cgsit.training.firstexample.rest;
+
+import at.cgsit.training.firstexample.chat.model.ChatMessage;
+import at.cgsit.training.firstexample.exceptions.ChatMessageNotFoundException;
+import at.cgsit.training.firstexample.properties.PropertiesSample;
+import at.cgsit.training.firstexample.services.ChatMessageServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/simplerest")
+public class SimpleRestController {
+
+  Logger logger = LoggerFactory.getLogger(SimpleRestController.class);
+
+  @Autowired
+  private PropertiesSample propertiesSample;
+
+  @GetMapping("/properties/{property}")
+  String getProperty(@PathVariable String property) {
+    logger.info("getProperty /properties called with value {}", property);
+    logger.info("Working Directory = " + System.getProperty("user.dir"));
+    return propertiesSample.getMyValue();
+  }
+}
